280c9822c30927584c331feaaf6f49b3813b86d7
Ricki <edicrist@tulane.edu>
2018-08-25 15:34:06 -0500

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,7 @@ CarrierWave.configure do |config|
       :provider => ‘AWS’,
       :aws_access_key_id => ENV[‘AWS_ACCESS_KEY_ID’],
       :aws_secret_access_key => ENV[‘AWS_SECRET_ACCESS_KEY’],
-      :region => ‘us-east-2’ 
-    }
+      :region => ‘us-east-2’ }
     config.fog_directory = ENV[‘S3_BUCKET_NAME’]
     config.storage = :fog
   else
