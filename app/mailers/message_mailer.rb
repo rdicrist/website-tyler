b3b17bbdf3b18870dd52a019fdec5c89d945fcb8
Ricki <edicrist@tulane.edu>
2018-08-30 18:59:59 -0500

    mailer stuff

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,18 @@
 class MessageMailer < ApplicationMailer
   require 'mailgun'
 
+  # def contact_me(message)
+  #   @body = message.body
+  #   mg_client = Mailgun::Client.new ENV['mailgun_api_key']
+  #   message_params = {:from => message.email,
+  #                     :to => ENV['email'],
+  #                     :subject => 'Message from tylerjfay.com',
+  #                     :text => message.body}
+  #   mg_client.send_message ENV['mailgun_domain'], message_params
+  # end
+
   def contact_me(message)
-    @body = message.body
-    mg_client = Mailgun::Client.new ENV['mailgun_api_key']
-    message_params = {:from => message.email,
-                      :to => ENV['email'],
-                      :subject => 'Message from tylerjfay.com',
-                      :text => message.body}
-    mg_client.send_message ENV['mailgun_domain'], message_params
+    @message = message
+    mail to: ENV['email'], subject: "Message from TylerJFay.com!"
   end
 end
